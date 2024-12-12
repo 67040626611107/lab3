e0171423c334b3b1e4f279df4710f4384fc0be92
Narin Suksawang <s6704062611107@email.kmutnb.ac.th>
2024-12-12 15:20:19 +0700

    Update and rename lab3no4 to lab3no4.c

--- lab3no4.c
+++ lab3no4.c
@@ -4,7 +4,7 @@ int main() {
   int score;
   scanf("%d",&score);
   if (score >= 40) {
-   printf("pass");
+   printf("Pass");
   } else if (score > 29) {
    printf("You can Test again");
   } else {
