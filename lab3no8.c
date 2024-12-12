e64f024cae81b5ec6869ba72a637c3eb4f3dc329
Narin Suksawang <s6704062611107@email.kmutnb.ac.th>
2024-12-12 15:52:01 +0700

    Create lab3no8.c

---
+++ lab3no8.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+int Hour,Minute,Second;
+int H,M,S;
+
+int Min() {
+  if (M > 59) {
+    Hour += 1;
+    M -= 60;
+  }
+  if (M > 59) {
+    Min();
+  }
+  return (0);
+}
+int Seconds() {
+  if (S > 59) {
+    Minute += 1;
+    S -= 60;
+  }
+  if (S > 59) {
+    Seconds();
+  }
+  return (0);
+}
+int main() {
+  printf("Hour : ");
+  scanf("%d",&H);
+  printf("Minute : ");
+  scanf("%d",&M);
+  printf("second : ");
+  scanf("%d",&S);
+  Seconds();
+  Second += S;
+  Min();
+  Minute += M;
+  if (Hour > 23) {
+    Hour = 0;
+  }
+  printf("Hour %d : Minute %d : Second : %d ",Hour,Minute,Second);
+  return (0);
+}
